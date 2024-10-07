8cf72bbbfdad55b07939794ef9632cdd37253299
Keiroru <124210421+Keiroru@users.noreply.github.com>
2024-10-07 10:24:23 +0200

    Add files via upload

---
+++ dolgozat/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using dolgozat;
+using System.Text;
+
+List<Verseny> versenyzo = [];
+using StreamReader sr = new(
+    path: "forras.txt",
+    encoding: Encoding.UTF8
+    );
+
+while (!sr.EndOfStream) versenyzo.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"{versenyzo.Count} versenyző fejezte be a versenyt");
+
+var elitbe = versenyzo.Where(v => v.Category == "elit").Count();
+Console.WriteLine($"versenyzők száma 'elit' kategóriában: {elitbe}");
+
+var nokatlageletkora = versenyzo.Where(v => v.Sex == "n").Average(v => DateTime.Now.Year - v.Birthdate);
+Console.WriteLine($"női versenyzők átlagéletkora: {nokatlageletkora}");
+
+var kerekparido = versenyzo.Sum(v => v.Cycling.TotalHours);
+Console.WriteLine($"a versenyzők kerékpározással töltött összideje: {kerekparido:0.00}");
+
+var elituszas = TimeSpan.FromSeconds(versenyzo.Where(v => v.Category == "elit junior").Average(v => v.Swimtime.TotalSeconds));
+Console.WriteLine($"átlagos úszási idő elit junior kategóriában: {elituszas}");
+
+
+var kategoriankent = versenyzo.GroupBy(v => v.Category);
+Console.WriteLine($"korkategóriánként a versenyt befejezők száma: ");
+foreach (var v in kategoriankent)
+{
+    Console.WriteLine($"{v.Key} : {v.Count()}");
+}
+
---
+++ dolgozat/Verseny.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat
+{
+    internal class Verseny
+    {
+
+        public string Name { get; set; }
+        public int Birthdate { get; set; }
+        public int Startnumber { get; set; }
+        public string Sex { get; set; }
+        public string Category { get; set; }
+        public TimeSpan Swimtime { get; set; }
+        public TimeSpan Depotime1 { get; set; }
+        public TimeSpan Cycling { get; set; }
+        public TimeSpan Depotime2 { get; set; }
+        public TimeSpan Running { get; set; }
+
+        public Verseny(string sor)
+        {
+            string[] sordb = sor.Split(';');
+            Name = sordb[0];
+            Birthdate = int.Parse(sordb[1]);
+            Startnumber = int.Parse(sordb[2]);
+            Sex = sordb[3];
+            Category = sordb[4];
+            Swimtime = TimeSpan.Parse(sordb[5]);
+            Depotime1 = TimeSpan.Parse(sordb[6]);
+            Cycling = TimeSpan.Parse(sordb[7]);
+            Depotime2 = TimeSpan.Parse(sordb[8]);
+            Running = TimeSpan.Parse(sordb[9]);
+        }
+
+        public override string ToString() =>
+            $"{Name} Születési év: {Birthdate} Rajtszám: {Startnumber} Nem: {Sex} Úszásidő {Swimtime} Első depó idő {Depotime1} bicikli idő {Cycling} Második depó {Depotime2} futás ideje {Running} ";
+}
+}
+
